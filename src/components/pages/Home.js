5ee8ce407b17ea6288cb4fded5ff24804ac5708e
Radoslaw <radsell@gmail.com>
2022-08-07 01:11:10 +0300

    added ETH balance

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,15 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
 import { useWeb3React } from '@web3-react/core'
 
 
 const Home = () => {
-    const { active, chainId, account } = useWeb3React();
+    const [balance,setBalance]= useState("")
+    const { active, chainId, account, library } = useWeb3React();
+    useEffect(() => {
+        library?.getBalance(account).then((result)=>{
+          setBalance(result/1e18)
+        })
+    },);
     return (
             <div className="container">
                 <div className="card connect-wallet">
@@ -11,8 +17,9 @@ const Home = () => {
                         Wallet Overview
                     </div>
                     <ul className="list-group list-group-flush">
-                        <li className="list-group-item">Connection Status: {active}</li>
+                        <li className="list-group-item">Active: {active}</li>
                         <li className="list-group-item">Account: {account}</li>
+                        <li className="list-group-item">ETH Balance: {balance}</li>
                         <li className="list-group-item">Network ID: {chainId}</li>
                     </ul>
                 </div>
